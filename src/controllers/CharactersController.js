7ab05ca41aee09cc5a8a5197b559b38e5d85c5d0
IGNACIO MATIAS LOPEZ ABRAHAM <46872351@ort>
2023-05-19 10:36:03 -0300

    added services, constrollers

--- src/controllers/CharactersController.js
+++ src/controllers/CharactersController.js
@@ -4,7 +4,50 @@ const router = Router();
 
 router.get('', async(req, res) => {
     const characters = await CharServices.getAll();
+    console.log(res);
     return res.status(200).json(characters);
+});
+
+router.post('', async(req, res) => {
+    try {
+        await CharServices.insert(req.body)
+        res.status(200).json({message: 'character inserted'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'insert failed'});
+    }
+});
+
+router.put('', async(req, res) => {
+    try {
+        await CharServices.update(req.body)
+        res.status(200).json({message: 'character updated'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'update failed'});
+    }
+});
+
+router.delete('/:id', async(req, res) => {
+    try {
+        await CharServices.delete(req.params.id)
+        res.status(200).json({message: 'character deleted'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'delete failed'});
+    }
+});
+
+router.get('/:id', async(req, res) => {
+    const r = await CharServices.getCharInfo(req.params.id)
+    console.log(res);
+    return res.status(200).json(r);
+});
+
+router.get('?name', async(req, res) => {
+    const r = await CharServices.getCharByName(req.query.name)
+    console.log(res);
+    return res.status(200).json(r);
 });
 
 export default router;
--- src/services/CharactersServices.js
+++ src/services/CharactersServices.js
@@ -14,4 +14,81 @@ export default class CharactersServices {
         }
         return returnEntity;
     }
+    static insert = async(character) => {
+        let returnEntity = null;
+        console.log(character);
+        const {Id, Image, Name, Age, Weight, History} = character;
+        let pool = await sql.connect(config);
+
+        try{
+            const request = new sql.Request(pool);
+
+            returnEntity = request
+            .input('Image', sql.NVarChar(MAX), Image)
+            .input('Name', sql.NVarChar(50), Name)
+            .input('Age', sql.Int, Age)
+            .input('Weight', sql.Float, Weight)
+            .input('History', sql.NVarChar(MAX), History)
+            .query('INSERT INTO Characters (Image, Name, Age, Weight, History) VALUES (@Image, @Name, @Age, @Weight, @History)')
+        }catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+    static update = async(character) => {
+        let returnEntity = null;
+        const {Id, Image, Name, Age, Weight, History} = character;
+        try{
+            const request = new sql.Request(pool);
+
+            returnEntity = request
+            .input('Image', sql.NVarChar(MAX), Image)
+            .input('Name', sql.NVarChar(50), Name)
+            .input('Age', sql.Int, Age)
+            .input('Weight', sql.Float, Weight)
+            .input('History', sql.NVarChar(MAX), History)
+                .query('UPDATE Characters SET Image = @Image, Name = @Name, Age = @Age, Weight = @Weight, History = @History WHERE Id = @Id');
+        }catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+    static delete = async(id) => {
+        let returnEntity = null;
+        try{
+            const request = new sql.Request(pool);
+            returnEntity = request
+            .input('Id', sql.Int, id)
+                .query('DELETE from Characters WHERE Id = @Id ');
+        }catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+    static getCharInfo = async(id) => {
+        let returnEntity = null;
+        try{
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+            .input('Id', sql.Int, id)
+                .query('SELECT C.*, MS.* FROM Characters C INNER JOIN [CharacterXMovies&Shows] X ON C.Id = X.fkCharacter INNER JOIN [Movies&Shows] MS ON MS.Id = X.fkMovieOrShow WHERE C.Id = 1');
+                returnEntity = result.recordsets[0][0];
+        }catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+    static getCharInfo = async(Name) => {
+        let returnEntity = null;
+        try{
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+            .input('Name', sql.Int, Name)
+                .query('SELECT * FROM Characters WHERE Name = @Name');
+                returnEntity = result.recordsets[0][0];
+        }catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
 }
