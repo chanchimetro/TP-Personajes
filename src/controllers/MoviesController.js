41627bc3d1d020b463781cc584f939a2849c6707
IGNACIO MATIAS LOPEZ ABRAHAM <46872351@ort>
2023-06-07 09:00:27 -0300

    added CRUD to Movies&Shows router

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -14,4 +14,37 @@ router.get('/:id', async(req, res) => {
     return res.status(200).json(movie);
 });
 
+router.post('', async(req, res) => {
+    try {
+        let r = await MoviesServices.insert(req.body)
+        console.log(r)
+        res.status(200).json({message: 'Movie/Show inserted!'});
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({error: 'insert failed!'});
+    }
+});
+
+router.put('', async(req, res) => {
+    try {
+        let r = await MoviesServices.update(req.body)
+        console.log(r)
+        res.status(200).json({message: 'Movie/Show updated!'});
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({error: 'update failed!'});
+    }
+});
+
+router.delete('/:id', async(req, res) => {
+    try {
+        let r = await MoviesServices.insert(req.params.id)
+        console.log(r)
+        res.status(200).json({message: 'Movie/Show deleted!'});
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({error: 'delete failed!'});
+    }
+});
+
 export default router;
--- src/services/MoviesServices.js
+++ src/services/MoviesServices.js
@@ -20,9 +20,64 @@ export default class MoviesServices {
         try{
             let pool = await sql.connect(config);
             let result = await pool.request()
+                .input('Id', sql.Int, id)
                 .query('SELECT C.*, MS.* FROM Characters C INNER JOIN [CharacterXMovies&Shows] X ON C.Id = X.fkCharacter INNER JOIN [Movies&Shows] MS ON MS.Id = X.fkMovieOrShow WHERE MS.Id = @id');
                 returnEntity = result.recordsets[0][0];
         }catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+
+    static insert = async(movie) => {
+        let returnEntity = null;
+        const {Id, Image, Title, ReleaseDate, Rating} = movie;
+        let pool = await sql.connect(config);
+        try {
+            const request = new sql.Request(pool);
+
+            returnEntity = request
+            .input('Image', sql.VarChar, Image)
+            .input('Title', sql.VarChar, Title)
+            .input('ReleaseDate', sql.Date, ReleaseDate)
+            .input('Rating', sql.Float, Rating)
+                .query('INSERT INTO [Movies&Shows] (Image, Title, ReleaseDate, Rating) VALUES (@Image, @Title, @ReleaseDate, @Rating)')
+        } catch(error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+
+    static update = async(movie) => {
+        let returnEntity = null;
+        const {Id, Image, Title, ReleaseDate, Rating} = movie;
+        let pool = await sql.connect(config);
+        try {
+            const request = new sql.Request(pool);
+
+            returnEntity = request
+            .input('Id', sql.Int, Id)
+            .input('Image', sql.VarChar, Image)
+            .input('Title', sql.VarChar, Title)
+            .input('ReleaseDate', sql.Date, ReleaseDate)
+            .input('Rating', sql.Float, Rating)
+                .query('UPDATE [Movies&Shows] SET Image = @Image, Title = @Title, ReleaseDate = @ReleaseDate, Rating = @Rating WHERE Id = @Id')
+        } catch(error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+
+    static delete = async(Id) => {
+        let returnEntity = null;
+        let pool = await sql.connect(config);
+        try {
+            const request = new sql.Request(pool);
+
+            returnEntity = request
+            .input('Id', sql.Int, Id)
+                .query('UPDATE [Movies&Shows] SET Image = @Image, Title = @Title, ReleaseDate = @ReleaseDate, Rating = @Rating WHERE Id = @Id')
+        } catch(error) {
             console.log(error);
         }
         return returnEntity;
